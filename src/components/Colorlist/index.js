d0171929dece2b05459e7c4dc8f74024e995af46
Léo Franca <leoarkiteto@gmail.com>
2020-09-07 11:56:14 +0100

    Created AddColorForm

--- src/components/Colorlist/index.js
+++ src/components/Colorlist/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 
 import Color from './Color';
 
-const Colorlist = ({ colors = [] }) => {
-  if (!colors.length) return <div>No Colors Listed.</div>;
+const Colorlist = ({
+  colors = [],
+  onRemoveColor = f => f,
+  onRateColor = f => f,
+}) => {
+  if (!colors.length) return <div>No Colors Listed. (Add a Color)</div>;
 
-  return (
-    <div>
-      {colors.map(color => (
-        <Color key={color.id} {...color} />
-      ))}
-    </div>
-  );
+  return colors.map(color => (
+    <Color
+      key={color.id}
+      {...color}
+      onRemove={onRemoveColor}
+      onRate={onRateColor}
+    />
+  ));
 };
 
 export default Colorlist;
--- src/components/StarRating/index.js
+++ src/components/StarRating/index.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Star from './Star';
 
 const createArray = length => [...Array(length)];
 
-const StarRating = ({ totalStars = 5, selectedStars = 0 }) => (
+const StarRating = ({ totalStars = 5, selectedStars = 0, onRate = f => f }) => (
   <>
     {createArray(totalStars).map((n, i) => (
-      <Star key={i} selected={selectedStars > i} />
+      <Star
+        key={i}
+        selected={selectedStars > i}
+        onSelect={() => onRate(i + 1)}
+      />
     ))}
     <p>
       {selectedStars} of {totalStars} stars
